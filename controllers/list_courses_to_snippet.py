522411e8e3ad7f964cb20c0e843dfcf488c961d1
simplify-m115 <m115@euroblaze.de>
2022-01-26 12:51:45 +0100

    poraka

--- controllers/list_courses_to_snippet.py
+++ controllers/list_courses_to_snippet.py
@@ -7,8 +7,8 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class CoursesStudents(CustomerPortal):
     @http.route("/courses/snippet_data", type='http', auth='user', website=True, csrf=False)
-    #TODO CHANGE ROUTE, MAKE SURE THAT DATA GETS TO THE SNIPPET
-    def list_courses_to_snippet(self, courses_id, **kwargs):
+    #TODO MAKE SURE THAT DATA GETS TO THE SNIPPET
+    def list_courses_to_snippet(self, **kwargs):
         if request.httprequest.method == 'GET':
             print("proba")
             courses = request.env['courses.courses'].sudo().search([])
