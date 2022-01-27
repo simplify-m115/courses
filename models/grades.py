99282587dfd0989553f801ecd899628d8cc6b838
simplify-m115 <m115@euroblaze.de>
2022-01-27 15:23:32 +0100

    cleared some comments from grades module

--- models/grades.py
+++ models/grades.py
@@ -19,9 +19,6 @@ class Grades(models.Model):
         readonly=True
     )
 
-    # student = fields.Many2one(
-    #     'courses')
-
     grade = fields.Selection([
         ("5","5"),
         ("6","6"),
@@ -32,37 +29,4 @@ class Grades(models.Model):
         string="Grade",
         required=False
     )
-
-    # # course = fields.Many2one(
-    # #     'courses.courses',
-    # #     ondelete = 'cascade',
-    # #     string = "Course",
-    # #     required = False,
-    # #     readonly = True
-    # # )
-    #
-    # course1 = fields.Many2one(
-    #     'courses.courses',
-    #     ondelete = 'cascade',
-    #     string = 'Course',
-    #     required = False,
-    #     readonly = True
-    # )
-    #
-    #
-    # student = fields.Many2one(
-    #     'courses.students',
-    #     ondelete="cascade",
-    #     string="Student",
-    #     help="Name of the student in the course.",
-    #     required=False,
-    #     readonly=True)
-    #
-    # grade = fields.Selection([
-    #     ("5", "5"), ("6", "6"),
-    #     ("7", "7"), ("8", "8"),
-    #     ("9", "9"), ("10", "10")],
-    #     string="Grade",
-    #     help="Students grade",
-    #     required=False)
 
